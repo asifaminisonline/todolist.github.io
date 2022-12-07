faa18cd945065d5c03610a4768fa756ab2892209
asifaminisonline <asifaminisonline@gmail.com>
2022-12-08 00:34:42 +0530

    add and remove functionality added to To-Do list project

--- dist/bundle.js
+++ dist/bundle.js
@@ -16,7 +16,7 @@
   \*************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"body {\\r\\n  margin: 0;\\r\\n  height: 100vh;\\r\\n  display: flex;\\r\\n  align-items: center;\\r\\n  font-family: sans-serif;\\r\\n  font-size: 16px;\\r\\n  font-weight: normal;\\r\\n}\\r\\n\\r\\nul {\\r\\n  padding: 0;\\r\\n  margin: 0;\\r\\n}\\r\\n\\r\\nli {\\r\\n  list-style: none;\\r\\n}\\r\\n\\r\\n.main-panel {\\r\\n  min-width: 40%;\\r\\n  max-width: 75%;\\r\\n  margin: auto;\\r\\n  display: grid;\\r\\n  box-shadow: 0 0 10px #888;\\r\\n}\\r\\n\\r\\n.list-title {\\r\\n  font-size: inherit;\\r\\n  margin: 0;\\r\\n}\\r\\n\\r\\n.add-input {\\r\\n  border: 1px solid black;\\r\\n  font-style: italic;\\r\\n}\\r\\n\\r\\n.add-input,\\r\\n.clear-btn {\\r\\n  border: none;\\r\\n}\\r\\n\\r\\n.list-title,\\r\\n.add-input,\\r\\nli,\\r\\n.clear-btn {\\r\\n  padding: 15px;\\r\\n  border-bottom: 1px solid #b5b5b5;\\r\\n}\\r\\n\\r\\n.clear-btn {\\r\\n  color: gray;\\r\\n  font-size: 14px;\\r\\n  border: none;\\r\\n}\\r\\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://to-do-list/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"body {\\r\\n  margin: 0;\\r\\n  height: 100vh;\\r\\n  display: flex;\\r\\n  align-items: center;\\r\\n  font-family: sans-serif;\\r\\n  font-size: 15px;\\r\\n  font-weight: normal;\\r\\n}\\r\\n\\r\\nul {\\r\\n  padding: 0;\\r\\n  margin: 0;\\r\\n}\\r\\n\\r\\nli {\\r\\n  list-style: none;\\r\\n}\\r\\n\\r\\n.main-panel {\\r\\n  min-width: 40%;\\r\\n  max-width: 75%;\\r\\n  margin: auto;\\r\\n  display: grid;\\r\\n  box-shadow: 0 0 10px #888;\\r\\n}\\r\\n\\r\\n.list-title {\\r\\n  font-size: inherit;\\r\\n  margin: 0;\\r\\n}\\r\\n\\r\\n.add-input {\\r\\n  border: 1px solid black;\\r\\n  font-style: italic;\\r\\n}\\r\\n\\r\\n.add-input,\\r\\n.clear-btn {\\r\\n  border: none;\\r\\n}\\r\\n\\r\\n.list-title,\\r\\n.add-input,\\r\\n.item-container,\\r\\n.clear-btn {\\r\\n  padding: 15px;\\r\\n  border-bottom: 1px solid #b5b5b5;\\r\\n  display: flex;\\r\\n}\\r\\n\\r\\n.clear-btn {\\r\\n  color: gray;\\r\\n  font-size: 14px;\\r\\n  border: none;\\r\\n}\\r\\n\\r\\n.complete {\\r\\n  text-decoration: line-through;\\r\\n  color: grey;\\r\\n}\\r\\n\\r\\n.text-area {\\r\\n  display: none;\\r\\n}\\r\\n\\r\\n.item-container {\\r\\n  justify-content: space-between;\\r\\n  gap: 5px;\\r\\n}\\r\\n\\r\\n.item-container span,\\r\\n.item-container textarea {\\r\\n  width: 200px;\\r\\n  flex-grow: 1;\\r\\n}\\r\\n\\r\\n.cancel-btn {\\r\\n  height: 20px;\\r\\n  width: 20px;\\r\\n}\\r\\n\\r\\n.edit-item {\\r\\n  display: inline;\\r\\n  font: inherit;\\r\\n  padding: 0;\\r\\n  height: 18px;\\r\\n  border: none;\\r\\n  outline: none;\\r\\n  resize: none;\\r\\n  cursor: text;\\r\\n  overflow: auto;\\r\\n}\\r\\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://to-do-list/./src/style.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -110,13 +110,43 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 
 /***/ }),
 
-/***/ "./src/main.js":
+/***/ "./src/crud.js":
 /*!*********************!*\
-  !*** ./src/main.js ***!
+  !*** ./src/crud.js ***!
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nconst toDoListItems = [\n  {\n    description: 'Finish new project',\n    completed: false,\n    index: 0,\n  },\n  {\n    description: 'Groceries',\n    completed: false,\n    index: 1,\n  },\n  {\n    description: 'Take Lunch',\n    completed: false,\n    index: 2,\n  },\n];\n\nfunction populateList() {\n  const todoList = document.getElementById('todo-list');\n\n  toDoListItems.forEach((item) => {\n    const listItem = document.createElement('li');\n    listItem.innerHTML = `\n    <input class=\"checkbox\" type=\"checkbox\" name=\"\" value=\"\">\n    <span>${item.description}</span>\n    `;\n    todoList.appendChild(listItem);\n  });\n}\n\npopulateList();\n\n\n//# sourceURL=webpack://to-do-list/./src/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"add\": () => (/* binding */ add),\n/* harmony export */   \"remove\": () => (/* binding */ remove),\n/* harmony export */   \"edit\": () => (/* binding */ edit),\n/* harmony export */   \"populateList\": () => (/* binding */ populateList),\n/* harmony export */   \"deleteAllCompleted\": () => (/* binding */ deleteAllCompleted)\n/* harmony export */ });\n/* harmony import */ var _todoList__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./todoList */ \"./src/todoList.js\");\n/* harmony import */ var _delete_btn_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./delete-btn.png */ \"./src/delete-btn.png\");\n\r\n\r\n\r\n// Add functionality\r\nfunction add(value) {\r\n  // Create new item\r\n  const newItem = new _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"](value);\r\n  // Update local storage\r\n  localStorage.setItem(\"todoList\", JSON.stringify(newItem.getList()));\r\n}\r\n\r\n// Remove Functionality\r\nfunction remove(index) {\r\n  _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list = _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list.filter((item) => item !== _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list[index]);\r\n  // Update indexes\r\n  _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list.forEach((item, i) => {\r\n    item.index = i;\r\n  });\r\n  // Update local storage\r\n  localStorage.setItem(\"todoList\", JSON.stringify(_todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list));\r\n}\r\n\r\nfunction edit(index, text) {\r\n  _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list[index].description = text;\r\n  // Update local storage\r\n  localStorage.setItem(\"todoList\", JSON.stringify(_todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list));\r\n}\r\n\r\n// Add items to UI\r\nfunction populateList() {\r\n  const todoList = document.getElementById(\"todo-list\");\r\n  todoList.innerHTML = \"\";\r\n\r\n  _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list.forEach((item) => {\r\n    const listItem = document.createElement(\"li\");\r\n    listItem.setAttribute(\"id\", item.index);\r\n    listItem.classList = \"item-container\";\r\n\r\n    listItem.innerHTML = `\r\n    <input class=\"checkbox\" type=\"checkbox\">\r\n    <span>${item.description}</span>\r\n    <textarea class=\"text-area\" maxlength=\"30\">${item.description}</textarea>\r\n    <img class=\"cancel-btn\" src=\"${_delete_btn_png__WEBPACK_IMPORTED_MODULE_1__}\">\r\n    `;\r\n\r\n    todoList.appendChild(listItem);\r\n\r\n    const checkbox = listItem.querySelector(\"input\");\r\n    const text = listItem.querySelector(\"span\");\r\n    const textInput = listItem.querySelector(\"textarea\");\r\n    const deleteButton = listItem.querySelector(\"img\");\r\n\r\n    // Update\r\n    checkbox.addEventListener(\"change\", () => {\r\n      const index = parseInt(listItem.id, 10);\r\n      _todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list[index].update();\r\n      text.classList.toggle(\"complete\");\r\n      textInput.classList.toggle(\"complete\");\r\n      localStorage.setItem(\"todoList\", JSON.stringify(_todoList__WEBPACK_IMPORTED_MODULE_0__[\"default\"].list));\r\n    });\r\n\r\n    // Edit functionality\r\n    text.addEventListener(\"click\", () => {\r\n      text.style.display = \"none\";\r\n      textInput.classList.toggle(\"edit-item\");\r\n    });\r\n\r\n    textInput.addEventListener(\"keydown\", (e) => {\r\n      // Follow value\r\n      text.innerHTML = textInput.value;\r\n\r\n      // Update list\r\n      const index = parseInt(listItem.id, 10);\r\n      edit(index, text.innerHTML);\r\n\r\n      if (e.code === \"Enter\") {\r\n        // Update UI\r\n        text.style.display = \"block\";\r\n        textInput.classList.toggle(\"edit-item\");\r\n      }\r\n    });\r\n\r\n    // Delete functionality\r\n    deleteButton.addEventListener(\"click\", () => {\r\n      // Update list\r\n      const index = parseInt(listItem.id, 10);\r\n      // Delete item\r\n      remove(index);\r\n      // Update UI\r\n      populateList();\r\n    });\r\n\r\n    if (item.complete) {\r\n      checkbox.checked = true;\r\n      text.classList = \"complete\";\r\n    }\r\n  });\r\n}\r\n\r\n// Clear completed functionality\r\nfunction deleteAllCompleted(todo) {\r\n  // Update list\r\n  todo.list = todo.list.filter((item) => item.complete === false);\r\n  // Update indexes\r\n  todo.list.forEach((item, i) => {\r\n    item.index = i;\r\n  });\r\n  // Update local storage\r\n  localStorage.setItem(\"todoList\", JSON.stringify(todo.list));\r\n}\r\n\n\n//# sourceURL=webpack://to-do-list/./src/crud.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _crud__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./crud */ \"./src/crud.js\");\n/* harmony import */ var _todoList__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./todoList */ \"./src/todoList.js\");\n\n\n\n\n// Window load\nconst list = JSON.parse(localStorage.getItem(\"todoList\"));\nif (list) {\n  list.forEach((item) => new _todoList__WEBPACK_IMPORTED_MODULE_2__[\"default\"](item.description, item.complete));\n}\n\n// Add\nconst addInput = document.getElementById(\"add-input\");\naddInput.addEventListener(\"keydown\", (event) => {\n  if (event.key === \"Enter\") {\n    (0,_crud__WEBPACK_IMPORTED_MODULE_1__.add)(addInput.value);\n    addInput.value = \"\";\n    (0,_crud__WEBPACK_IMPORTED_MODULE_1__.populateList)();\n  }\n});\n\n// Delete all completed\nconst clearButton = document.getElementById(\"clear-btn\");\nclearButton.addEventListener(\"click\", () => {\n  (0,_crud__WEBPACK_IMPORTED_MODULE_1__.deleteAllCompleted)(_todoList__WEBPACK_IMPORTED_MODULE_2__[\"default\"]);\n  (0,_crud__WEBPACK_IMPORTED_MODULE_1__.populateList)();\n});\n\n// Populate UI\n(0,_crud__WEBPACK_IMPORTED_MODULE_1__.populateList)();\n\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/todoList.js":
+/*!*************************!*\
+  !*** ./src/todoList.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ ToDo)\n/* harmony export */ });\nclass ToDo {\r\n  static list = [];\r\n\r\n  constructor(description, complete = false) {\r\n    this.description = description;\r\n    this.complete = complete;\r\n    this.index = ToDo.list.length;\r\n    ToDo.list.push(this);\r\n    this.getList = () => ToDo.list;\r\n  }\r\n\r\n  update() {\r\n    if (this.complete) {\r\n      this.complete = false;\r\n    } else {\r\n      this.complete = true;\r\n    }\r\n  }\r\n}\r\n\n\n//# sourceURL=webpack://to-do-list/./src/todoList.js?");
+
+/***/ }),
+
+/***/ "./src/delete-btn.png":
+/*!****************************!*\
+  !*** ./src/delete-btn.png ***!
+  \****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"aa0a742c736cf9eb8a1e.png\";\n\n//# sourceURL=webpack://to-do-list/./src/delete-btn.png?");
 
 /***/ })
 
@@ -171,6 +201,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -187,12 +229,34 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	__webpack_require__("./src/todoList.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/crud.js");
 /******/ 	
 /******/ })()
 ;
